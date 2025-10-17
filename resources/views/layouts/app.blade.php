17b08fb61333351df3ce12f4ee223aefec2681df
Mersmith <mersmith14@gmail.com>
2025-10-16 19:56:24 -0500

    avance

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -11,6 +11,9 @@
 
     <!-- Estilos generales -->
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.css" />
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.2/css/all.min.css"
+        integrity="sha512-SnH5WK+bZxgPHs44uWIX+LLJAJ9/2PkPKZ5QiAj6Ta86w+fsb2TkcmfRyVX3pBnMFcV7oQPJkl9QevSCWr3W6A=="
+        crossorigin="anonymous" referrerpolicy="no-referrer" />
 
     <!-- SCRIPTS -->
     @vite(['resources/css/app.css', 'resources/js/app.js'])
@@ -30,6 +33,7 @@
         @yield('contenido')
     </main>
 
+    @include('partials.footer')
 </body>
 
 </html>
---
+++ resources/views/partials/footer.blade.php
@@ -0,0 +1,24 @@
+<footer class="footer">
+    <div class="footer-contact">
+        <h3> Central Telefónica</h3>
+        <p><i class="fa-solid fa-phone"></i>924 218 321</p>
+    </div>
+
+    <div class="footer-main">
+        <div class="footer-info">
+            <h2>Multiservicios Fumisoluciones SAC</h2>
+            <p>FUMIGACIÓN, DESINFECCIÓN, LIMPIEZA Y SANITIZACIÓN</p>
+        </div>
+
+        <div class="footer-socials">
+            <a href="#" target="_blank"><i class="fa-brands fa-facebook-f"></i></a>
+            <a href="#" target="_blank"><i class="fa-brands fa-instagram"></i></a>
+            <a href="#" target="_blank"><i class="fa-brands fa-tiktok"></i></a>
+        </div>
+    </div>
+
+    <div class="footer-bottom">
+        <p>© <strong>Multiservicios Fumisoluciones SAC</strong>.</p>
+        <p>Todos los derechos reservados.</p>
+    </div>
+</footer>
